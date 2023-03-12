ee51da9b54f8f291379c0b12d1b32f96fb403860
789sandeep <108711927+789sandeep@users.noreply.github.com>
2023-03-12 16:30:47 +0530

    Update Q1.py

--- 22 feb Assingment/Q1.py
+++ 22 feb Assingment/Q1.py
@@ -1,5 +1,5 @@
 # Q1. Create a Pandas Series that contains the following data: 4, 8, 15, 16, 23, and 42. Then, print the series.
-import panda as pd
+import pandas as pd
 data=[4,8,15,16,23,42]
 df=pd.Series(data)
 print(df)
